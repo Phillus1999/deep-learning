ef5a0ae2cfe140e5049195f6133192fb844b19b6
Philipp Fels <philippfels@hotmail.de>
2023-10-28 01:06:35 +0200

    add pytorch_perceptron_v1

--- uebungen/pytorch_percepron.py
+++ uebungen/pytorch_percepron.py
@@ -1,63 +1,90 @@
 import torch
-from sklearn.datasets import make_blobs
+from sklearn.datasets import make_blobs, make_moons
 import matplotlib.pyplot as plt
 
 
 class Perceptron:
     # implement perceptron using pytorch
-    def __init__(self, num_inputs=2, learning_rate=0.01):
+    def __init__(self, num_inputs=2, learning_rate=0.01, plot=False):
         self.num_inputs = num_inputs
-        self.weights = torch.ones(self.num_inputs, 1)
+        self.weights = torch.ones(self.num_inputs)
         self.learning_rate = learning_rate
         self.bias = 1
-    def infer(self, x):
-        """
-        :param x: input tensor
-        :return: output tensor after activation_function
-        """
-        weighted_sum = torch.mm(x, self.weights) + self.bias
-        return torch.sign(weighted_sum)
 
-    def update_weights(self, x, y):
+    def forward(self, X):
         """
-        :param x: input tensor
-        :param y: ground truth
-        :param y_hat: prediction
-        :return: None
+        berechnet die prediction des perceptrons
+        :param X: data tensor
+        :return: label tensor
         """
-        y_pred = self.infer(x).squeeze(1)
-        print(y_pred)
-        print(y)
-        error = y - y_pred
-        # Update weights
-        weight_update = torch.mm(x.T, error)  # shape (num_features, 1)
-        self.weights += self.learning_rate * weight_update
-        # Update bias
-        bias_update = torch.sum(error, dim=0)  # shape (1,)
-        self.bias += self.learning_rate * bias_update
+        # TODO: verstehen warum das genau so funktioniert
+        # hier ist mm nicht das gleiche wie matmul ?!
+        # torch.sign gibt -1 oder 1 zurück je nachdem ob das argument negativ oder positiv ist
+        # nutzt also gradient descent ?!
+        # TODO: gradient descent verstehen
+        return torch.sign(torch.matmul(X, self.weights) + self.bias)
 
-    def train(self, x, y, epochs=10):
+    def train(self, X, y, epochs=10):
         """
-        :param x: input tensor
-        :param y: ground truth
-        :param epochs: number of training epochs
+        passt die gewichte und das bias des perceptrons an
+        :param X: data tensor
+        :param y: label tensor
+        :param epochs: number of epochs
         :return: None
         """
         for epoch in range(epochs):
-            self.update_weights(x, y)
+            # erstellt einen tensor der selben länge wie X und füllt ihn mit den predictions
+            y_pred = self.forward(X)
+            # erstellt einen tensor der selben länge wie y_pred und füllt ihn mit den fehlern
+            error = y - y_pred
+            # TODO: vielleicht hier auch vektorisieren und nicht iterativ ?
+            for i in range(len(X)):
+                # passe die gewichte an
+                self.weights = self.weights + self.learning_rate * error[i] * X[i]
+                # passe den bias an
+                self.bias = self.bias + self.learning_rate * error[i]
+
 
 if __name__ == '__main__':
-    X, y = make_blobs(n_samples=100, centers=2, n_features=2, random_state=1)
-    X = torch.from_numpy(X).float()
-    y = torch.from_numpy(y).float()
-    plt.figure()
-    plt.subplot(1,2,1)
-    plt.scatter(X[:, 0], X[:, 1], c=y)
-    plt.title("Ground Truth")
-    perceptron = Perceptron()
-    perceptron.train(X, y)
-    y_pred = perceptron.infer(X)
-    plt.subplot(1,2,2)
-    plt.scatter(X[:, 0], X[:, 1], c=y_pred)
-    plt.title("Prediction")
+    # TODO: darstellung der entscheidungsgrenze über epochen darstellen
+
+    # erstelle die daten (2 features, 100 samples)
+    data = make_moons(n_samples=100)
+
+    # erstelle die tensoren aus den numpy arrays
+    features = torch.from_numpy(data[0]).float()
+    labels = torch.from_numpy(data[1]).float()
+
+    # erstelle das perceptron
+    perc = Perceptron()
+
+    # trainiere das perceptron
+    perc.train(features, labels, epochs=50)
+
+    # erstelle einen Plot um das Ergebnis zu visualisieren
+    fig = plt.figure(figsize=(12, 5))
+    # setze x und y limits
+    xlim = (features[:, 0].min() - 0.1, features[:, 0].max() + 0.1)
+    ylim = (features[:, 1].min() - 0.1, features[:, 1].max() + 0.1)
+
+    # plotte die daten mit richtigen labels
+    ax = fig.add_subplot(1,2,1)
+    ax.set_xlim(xlim)
+    ax.set_ylim(ylim)
+    plt.scatter(features[:, 0], features[:, 1], c=labels)
+    plt.title('Original Labels')
+
+    # plotte die daten mit den predictions
+    ax = fig.add_subplot(1,2,2)
+    ax.set_xlim(xlim)
+    ax.set_ylim(ylim)
+    plt.scatter(features[:, 0], features[:, 1], c=perc.forward(features))
+    plt.title('Predicted Labels')
+
+    # plotte die entscheidungsgrenze
+    x1 = torch.tensor([features[:, 0].min(), features[:, 0].max()])
+    x2 = -(perc.weights[0] * x1 + perc.bias) / perc.weights[1]
+    plt.plot(x1, x2, '--r')
+
+
     plt.show()
